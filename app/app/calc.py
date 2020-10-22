8d5901a7ee1ce1149f7fbef87bbb4d6dce7029f0
datakobs0 <datakobs0@gmail.com>
2020-10-22 17:33:15 +0200

    added custom user model

---
+++ app/app/calc.py
@@ -0,0 +1,8 @@
+
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return y - x
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_successful(self):
+        email = "test@gmail.com"
+        password = "123456"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        email = "test@gmail.com"
+        user = get_user_model().objects.create_user(email, "123456")
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "123456")
